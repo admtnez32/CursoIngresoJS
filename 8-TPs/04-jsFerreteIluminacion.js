5fdb16e68e6a17ff69fd427b3acd60de1d23a8d1
admtnez32 <admtnez32@gmail.com>
2021-02-10 22:21:55 -0300

    Clase 6

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -22,7 +22,9 @@ function CalcularPrecio ()
  	var lamparas;
  	var lamparasParseado;
  	var empresa;
+ 	var descuento = 0;
  	var precioLampara = 35;
+ 	var precio;
  	var precioFinal;
  	var precioIIBB;
 
@@ -30,19 +32,87 @@ function CalcularPrecio ()
  		lamparasParseado = parseInt(lamparas);
  		empresa = Marca.value;
 
- 			if(lamparasParseado > 5)
+  			if(lamparasParseado > 5)
  			{
- 				precioFinal = lamparasParseado*precioLampara/2;
-
- 				txtIdprecioDescuento.value = precioFinal;
+ 				descuento = 50;
  			}
  			else 
- 				if(lamparasParseado == 5 && empresa == "ArgentinaLuz")
+ 				if(lamparasParseado == 5)
  				{
- 					precioFinal = lamparasParseado*precioLampara*0.6;
+ 					if(empresa == "ArgentinaLuz")
+ 					{
 
- 					txtIdprecioDescuento.value = precioFinal;
- 				}	
+ 							descuento = 40;
+ 							//MArca ArgentinaLuz %40
+
+ 							//precioFinal = lamparasParseado*precioLampara*0.6;
+
+ 							//txtIdprecioDescuento.value = precioFinal;
+ 					}
+ 					else
+ 					{
+ 						descuento = 30;
+ 						//Otra marca %30
+ 					}
+ 				}
+ 				else
+ 					if(lamparasParseado == 4)
+ 					{
+ 						if(empresa == "ArgentinaLuz" || empresa == "FelipeLamparas")
+ 						{
+ 							descuento = 25;
+ 							// Marca Arg o Felipe %25
+ 						}
+ 						else
+ 						{
+ 							descuento = 20;
+ 							//Otra Marca %20
+ 						}
+ 					}
+ 					else
+ 					{
+ 						if(lamparasParseado == 3)
+ 						{
+ 							if(empresa == "ArgentinaLuz")
+ 							{
+ 								//%15
+ 								descuento = 15;
+ 							}
+ 							else
+ 							{
+ 								if(empresa == "FelipeLamparas")
+ 								{
+ 									descuento = 10;
+ 									//%10
+ 								}
+ 								else
+ 								{
+ 									descuento = 5;
+ 									//Otra MArca %5
+ 								}
+ 							}
+ 						}
+ 					}
+
+ 					precio = lamparasParseado*precioLampara;
+ 					precioFinal = precio-(precio*descuento/100);
+
+ 					
+
+ 					if(precioFinal > 120)
+ 					{
+ 						precioIIBB = precioFinal*1.1;
+ 						txtIdprecioDescuento.value = "El precio final con descuento + IIBB = $"+precioIIBB.toFixed(2)+ " \n IIBB = $"+ (precioFinal/10).toFixed(2);
+ 					}
+ 					else
+ 					{
+ 						txtIdprecioDescuento.value = precioFinal.toFixed(2);
+ 					}
+}
+
+
+
+ 				/*	
  					else 
  						if(lamparasParseado == 5 && empresa != "ArgentinaLuz")
  						{
@@ -97,4 +167,4 @@ function CalcularPrecio ()
  			precioIIBB = precioFinal*1.1;
  			txtIdprecioDescuento.value = "El precio final con descuento + IIBB = $"+precioIIBB+ " \n IIBB = $"+ precioFinal/10;
  		}
-}
+}*/
